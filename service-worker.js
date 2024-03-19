66b0ba23d677150fcc6686afcd4fa257e5f0db5b
krisada <krisada.m@ku.th>
2024-03-19 09:51:39 +0700

    edit pwa

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 	  caches.open('v1').then(function(cache) {
 		return cache.addAll([
-		  '/index.html',
-		  '/styles.css',
-		  '/script.js'
+		  'index.html',
+		  'styles.css',
+		  'script.js'
 		]);
 	  })
 	);
