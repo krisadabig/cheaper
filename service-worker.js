cb3f794cc2705a4b955027eb11c3994ead3d8894
krisada <krisada.m@ku.th>
2024-03-27 11:07:47 +0700

    add Icon

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,12 @@
 // service-worker.js
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
+workbox.routing.registerRoute(
+	  ({request}) => request.destination === 'image',
+  new workbox.strategies.NetworkFirst()
+);
 self.addEventListener('install', function(event) {
 	event.waitUntil(
 	  caches.open('v1').then(function(cache) {
